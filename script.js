e7015c861fc02fe42715a4bb94662f99e9788eda
hima_khaitan <himanshukhaitan108@gmail.com>
2021-03-05 09:23:09 +0530

    Active Player Changed

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ roundScore = 0;
 activePlayer = 0;
 
 
-
-// document.querySelector('#current--' + activePlayer).textContent = dice;
-// document.querySelector('#current--' + activePlayer).innerHTML = '<em>' + dice +'</em>';
-
-// var x = document.querySelector('#score--0').textContent;
-// console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
+
+// Scores set to Zero
+document.getElementById('current--0').textContent  = '0';
+document.getElementById('score--0').textContent  = '0';
+document.getElementById('current--1').textContent  = '0';
+document.getElementById('score--1').textContent  = '0';
+
 
 document.querySelector('.btn--roll').addEventListener('click', function () {
     // Random Number
@@ -23,4 +23,21 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
     diceDOM.src = 'dice-' + dice + '.png';
     // update the round scores if the number is NOT 1
      
+    if (dice !== 1) {
+        // Add Score
+        roundScore += dice;
+        document.querySelector('#current--' + activePlayer).textContent = roundScore;
+    } else {
+        // Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        console.log(activePlayer)
+    }
 });
+
+
+
+// document.querySelector('#current--' + activePlayer).textContent = dice;
+// document.querySelector('#current--' + activePlayer).innerHTML = '<em>' + dice +'</em>';
+
+// var x = document.querySelector('#score--0').textContent;
+// console.log(x);
