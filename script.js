5a3742ad51b7260ae3fa09ca35b9d519cb142c1b
hima_khaitan <himanshukhaitan108@gmail.com>
2021-03-05 08:56:49 +0530

    Dice Nos Displayed

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() *6 ) + 1;
-console.log(dice)
 
-document.querySelector('#current--' + activePlayer).textContent = dice;
+
+// document.querySelector('#current--' + activePlayer).textContent = dice;
 // document.querySelector('#current--' + activePlayer).innerHTML = '<em>' + dice +'</em>';
 
 // var x = document.querySelector('#score--0').textContent;
 // console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn--roll').addEventListener('click', function () {
+    // Random Number
+    var dice = Math.floor(Math.random() *6 ) + 1;
+    // Display the results
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    // update the round scores if the number is NOT 1
+     
+});
